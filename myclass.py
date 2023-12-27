40208a784a662774b1d6731d7dea79c8f124f7dd
Daniel Rivas <drivas8891@gmail.com>
2023-12-27 12:54:41 -0800

    Initial commit

---
+++ myclass.py
@@ -0,0 +1,16 @@
+# Python output console uses the Print() function
+# You can print multiple strings separated by commas
+
+first_name = "Daniel"
+last_name = "Rivas"
+
+print("Hello World!")
+print("I am", first_name, end='\n')
+
+full_name = first_name + " " + last_name
+
+print(full_name, end='\n')
+
+
+# Okay now lets make AI
+
